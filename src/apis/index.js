09de5e65f5f4890ac0c8d47a1a992be433ce555d
Xerrion <lassexl20@gmail.com>
2019-06-24 18:30:31 +0200

    Added Private API key, only access from specific domain

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://hidden-woodland-32976.herokuapp.com",
   headers: {
-    Authorization: "Bearer " + process.env.REACT_APP_HELLO
+    Authorization: "Bearer a47d8081cc954f1f385b1620a56812819454df10"
   }
 });
